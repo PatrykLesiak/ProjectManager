842053f5d8411447112850dad640b89846baa503
Patryk <patryk.lesiakk@gmail.com>
2015-05-20 23:10:43 +0200

    Example of making connection to database works

--- ProjectManager-ejb/src/main/java/pl/agh/wfiis/NewSessionBean.java
+++ ProjectManager-ejb/src/main/java/pl/agh/wfiis/NewSessionBean.java
@@ -4,6 +4,9 @@
  */
 package pl.agh.wfiis;
 
+import java.util.Iterator;
+import java.util.List;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ejb.LocalBean;
 
@@ -15,15 +18,28 @@ import javax.ejb.LocalBean;
 @LocalBean
 public class NewSessionBean {
 
+    @EJB
+    private UserFacade userFacade;
+    
     private String lol2;
     
     public NewSessionBean() {
         lol2 = "test z lolem2 sie udal";
     }
-
     
     public String test() {
-        return lol2;
+        User newUser = new User();
+        newUser.setUserid(3);
+        newUser.setFirstname("Andrzej");
+        userFacade.create(newUser);
+        
+        String out = "";
+        List users = userFacade.findAll();
+        for (Iterator it = users.iterator(); it.hasNext();) {
+            User user = (User)it.next();
+            out += user.getFirstname() + ", ";
+        }
+        return out;
     }
 
     // Add business logic below. (Right-click in editor and choose
